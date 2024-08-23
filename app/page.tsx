32a86adbff2d70c9870ae39eb1601644643ebf2f
gilbertmao <gilbertmao3266123@gmail.com>
2024-08-22 20:41:07 -0400

    Initial Stubbing for Personal Website

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import { Grid } from "@/components/Grid"
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        {/* <FloatingNav navItems={[[{name: 'Home', link: '/', icon: <FaHome />}]]} /> */}
+        <Hero />
+        <Grid />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
